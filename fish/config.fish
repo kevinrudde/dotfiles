2271c3eaec948a83b6780802e952fee2e1566d7f
Kevin Rudde <k.rudde@shopware.com>
2022-02-08 09:48:35 +0100

    Added custom alias cdcore

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,5 @@
 # Change variables hotkey to ctrl+alt+v, in order to allow pasting with ctrl+v
 fzf_configure_bindings --variables=\e\cV
 
+# Custom aliases
+alias cdcore="cd ~/Projects/Shopware/shopware-business-platform/Components/Core/"
