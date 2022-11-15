22fab577dadb3c82ffac55839791c72e55f2395e
Kevin Rudde <krudde@proton.me>
2022-11-15 09:55:07 +0100

    Updated polybar script

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -6,11 +6,13 @@ polybar-msg cmd quit
 # Wait until the processes have been shut down
 while pgrep -u $USER -x polybar; do sleep 0.1; done
 
-# Launch polybar on all displays
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload main &
-  done
-else
-  polybar --reload example &
-fi
+PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
+OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
+
+MONITOR=$PRIMARY polybar --reload main &
+sleep 1
+
+# Launch on all other monitors
+for m in $OTHERS; do
+ MONITOR=$m polybar --reload main &
+done
